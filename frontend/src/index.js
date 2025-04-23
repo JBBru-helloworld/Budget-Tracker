4b176c27c2be63872a142129eb26d0713535af20
JBBru-helloworld <jb.brubusiness3@gmail.com>
2025-04-23 23:02:38 +0800

    Add console log for error checking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// frontend/src/index.js
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// At the top of your index.js
+const originalConsoleError = console.error;
+console.error = (...args) => {
+  if (args[0] && args[0].includes("Warning:")) {
+    originalConsoleError(...args);
+  }
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
