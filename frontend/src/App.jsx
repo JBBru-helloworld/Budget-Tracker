1f03f864a64de6148f2248beb4db86be019ae639
JBBru-helloworld <jb.brubusiness3@gmail.com>
2025-04-25 01:32:53 +0800

    Import index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes";
+import "./index.css"; // Import the CSS file
 
 // Main App component
 function App() {
