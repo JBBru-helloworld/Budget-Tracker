b3859127f781ac828c00fc2d2188be7eef75d4e8
JBBru-helloworld <jb.brubusiness3@gmail.com>
2025-04-28 00:06:34 +0800

    Fix API URL

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 // src/api/auth.js
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 export const registerUser = async (userData) => {
   try {
