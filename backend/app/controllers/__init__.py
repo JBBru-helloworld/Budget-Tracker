97f7353a1e663e77d6645caead95070e63bd76ce
Joshua Bonham <150272592+JBBru-helloworld@users.noreply.github.com>
2025-05-02 00:08:00 +0800

    Initialise controllers

---
+++ backend/app/controllers/__init__.py
@@ -0,0 +1,19 @@
+from . import auth_controller
+from . import category_controller
+from . import receipt_controller
+from . import user_profile_controller
+from . import analytics_controller
+from . import tips_controller
+from . import settings_controller
+from . import scan_controller
+
+__all__ = [
+    'auth_controller',
+    'category_controller',
+    'receipt_controller',
+    'user_profile_controller',
+    'analytics_controller',
+    'tips_controller',
+    'settings_controller',
+    'scan_controller'
+] 
