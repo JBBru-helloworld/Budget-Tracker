29b20a95d2f95c86e4b7e66667a93f6d165cb98a
Joshua Bonham <150272592+JBBru-helloworld@users.noreply.github.com>
2025-05-09 00:09:24 +0800

    Remove scan_controller as there is no scan controller file anymore

--- backend/app/controllers/__init__.py
+++ backend/app/controllers/__init__.py
@@ -5,7 +5,6 @@ from . import user_profile_controller
 from . import analytics_controller
 from . import tips_controller
 from . import settings_controller
-from . import scan_controller
 
 __all__ = [
     'auth_controller',
@@ -14,6 +13,5 @@ __all__ = [
     'user_profile_controller',
     'analytics_controller',
     'tips_controller',
-    'settings_controller',
-    'scan_controller'
+    'settings_controller'
 ] 
